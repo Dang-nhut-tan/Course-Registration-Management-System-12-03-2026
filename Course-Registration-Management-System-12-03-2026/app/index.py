34a80115b971ddb752a962fb52307d63a91e14ff
Dang Nhut Tan <tan128340@gmail.com>
2026-03-13 09:47:00 +0700

    Add login UI, support page link and forgot password page

--- Course-Registration-Management-System-12-03-2026/app/index.py
+++ Course-Registration-Management-System-12-03-2026/app/index.py
@@ -1,9 +1,21 @@
+import os
+import sys
+
 from flask import render_template
-from app import app
+
+if __package__ in (None, ""):
+    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+    from app import app
+else:
+    from app import app
 
 @app.route("/")
 def login():
     return render_template("login.html")
+
+@app.route("/forgot-password")
+def forgot_password():
+    return render_template("forgot-password.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
